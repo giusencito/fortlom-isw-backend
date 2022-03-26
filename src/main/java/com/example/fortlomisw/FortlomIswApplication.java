09e0c73ab31d65ef0e10e93869a7c396d1694831
giusencito <66499088+giusencito@users.noreply.github.com>
2022-03-26 00:33:41 -0500

    Principal Content

---
+++ src/main/java/com/example/fortlomisw/FortlomIswApplication.java
@@ -0,0 +1,13 @@
+package com.example.fortlomisw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortlomIswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortlomIswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fortlomisw/FortlomIswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fortlomisw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortlomIswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
