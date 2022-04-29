c859a0da6ff81338d93b92700094dcd6c6a9b0cd
giusencito <66499088+giusencito@users.noreply.github.com>
2022-04-29 15:08:30 -0500

    configuration to run mvn test

--- src/test/java/com/example/fortlomisw/FortlomIswApplicationTests.java
+++ src/test/java/com/example/fortlomisw/FortlomIswApplicationTests.java
@@ -3,11 +3,9 @@ package com.example.fortlomisw;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FortlomIswApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
